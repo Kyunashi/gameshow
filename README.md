c98fcbd00f6519733fc74d7b1aa6c6ae538e786c
quentin <qlauterbach.dev@gmail.com>
2023-12-17 19:54:55 +0100

    Initialize Project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gameshow
 Gameshow Webapp to host a gameshow that features players, gamemaster and a  scoring system (maybe later also login, highscore tracking and more).  
+
+Backend:
+Java Spring application that provides the backend api for the gameshow. This Api will be used by a webserver (maybe also mobile Apps later)
---
+++ src/main/java/com/kyunashi/gameshow/GameshowApplication.java
@@ -0,0 +1,13 @@
+package com.kyunashi.gameshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameshowApplication.class, args);
+	}
+
+}
