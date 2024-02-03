91c220e2af922d290d3ed732091931f546ace051
quentin <qlauterbach.dev@gmail.com>
2024-02-03 01:48:02 +0100

    Ignore

--- src/main/java/com/kyunashi/gameshow/jeopardy/Jeopardy.java
+++ src/main/java/com/kyunashi/gameshow/jeopardy/Jeopardy.java
@@ -1,11 +1,13 @@
 package com.kyunashi.gameshow.jeopardy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Jeopardy {
 
 
 
-    ArrayList<ArrayList<Tile>> jeopBoard = new ArrayList<>();
+    Map<String, ArrayList<Tile>> jeopBoard = new HashMap<>();
 
 }
