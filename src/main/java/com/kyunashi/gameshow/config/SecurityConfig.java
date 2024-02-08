4510ce4385ffe6c909729ddaa8c3c103e41cbba9
quentin <qlauterbach.dev@gmail.com>
2024-02-08 01:51:38 +0100

    Add cors configuration

--- src/main/java/com/kyunashi/gameshow/config/SecurityConfig.java
+++ src/main/java/com/kyunashi/gameshow/config/SecurityConfig.java
@@ -24,6 +24,11 @@ import org.springframework.security.web.context.HttpSessionSecurityContextReposi
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 /**
  * Class to configure spring security rules which spring then inforces
@@ -68,6 +73,18 @@ public class SecurityConfig {
                 new HttpSessionSecurityContextRepository());
     }
 
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     /**
      * Filter chain enforces security rules
      * these rules are configured with this method
@@ -81,6 +98,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .userDetailsService(userDetailsService)
                 .securityContext((securityContext) -> securityContext
                         .requireExplicitSave(true)
--- src/main/java/com/kyunashi/gameshow/controller/AuthController.java
+++ src/main/java/com/kyunashi/gameshow/controller/AuthController.java
@@ -7,8 +7,6 @@ import com.kyunashi.gameshow.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,31 +15,33 @@ import org.springframework.web.bind.annotation.*;
  * - login, creating user via signup, changing
  */
 @Controller
+@CrossOrigin
 @RequestMapping("/api/auth")
 @AllArgsConstructor
 public class AuthController {
 
-    //  TODO UPDATE METHODS TO ONLY RETURN HTTP STATUS CODE, MESSAGES WILL BE DONE BY FRONTEND?
     private final AuthService authService;
     private final UserService userService;
 
+
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody SignupRequest signupRequest) {
+    public void signup(@RequestBody SignupRequest signupRequest, HttpServletResponse response) {
 
 
         if (userService.existsByUsername(signupRequest.getUsername())) {
-            return new ResponseEntity<>("Username is already taken", HttpStatus.CONFLICT);
+            response.setStatus(409);
         }
         authService.signup(signupRequest);
-        return new ResponseEntity<>("Signup successful", HttpStatus.OK);
+        response.setStatus(200);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest, HttpServletRequest request, HttpServletResponse response) {
+    public void login(@RequestBody LoginRequest loginRequest, HttpServletRequest request, HttpServletResponse response) {
         boolean successful = authService.login(loginRequest, request, response);
-        if (!successful)
-            return new ResponseEntity<>("The Combination of password and username is incorrect", HttpStatus.CONFLICT);
-        return new ResponseEntity<>("Login Successful for username " + loginRequest.getUsername(), HttpStatus.OK);
+        if (!successful) {
+            response.setStatus(409);
+        }
+        response.setStatus(200);
     }
 
 
